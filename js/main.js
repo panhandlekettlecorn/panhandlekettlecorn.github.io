61303de1464dc9790000e1b89c00571937db3064
Samuel Ballard <sballard19@gmail.com>
2017-03-06 04:15:39 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 
+$('.main-slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
